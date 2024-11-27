c42be22234f8fbc103d64253039fab1df1e1a48b
otmusprime <0000108446990xsp@al.educacao.sp.gov.br>
2024-11-26 21:00:33 -0300

    Add files via upload
    porque gostamos de futebol

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'futebol',
+    'O que é o futebol?',
+    'O futebol é um esporte coletivo de 11 jogadores cada time'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual foi o primero clube brasileiro a ser campeão mundial?',
+    'santos'
+)
+
+criaCartao(
+    'Programação',
+    'o palmeiras tem mundial galera?',
+    'nãoooooooo'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'qual o maior time ingles?',
+    'livepool'
+)
